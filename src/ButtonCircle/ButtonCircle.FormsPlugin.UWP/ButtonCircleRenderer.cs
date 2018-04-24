b251f89edb1693a8b704a441e4b6ecb57dd3ce51
Wilson Vargas <wilsonvargas_6@outlook.com>
2018-04-23 19:50:39 -0500

    fixed a NRE that occured when disposing objects in UWP.

--- src/ButtonCircle/ButtonCircle.FormsPlugin.UWP/ButtonCircleRenderer.cs
+++ src/ButtonCircle/ButtonCircle.FormsPlugin.UWP/ButtonCircleRenderer.cs
@@ -27,23 +27,26 @@ namespace ButtonCircle.FormsPlugin.UWP
         protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
         {
             base.OnElementChanged(e);
-            if (Control == null || Element == null)
-                throw new InvalidOperationException(String.Format("Cannot convert {0} into {1}", Element.Text, typeof(Icon))); ;
-
-            CreateCircle();
-            if (!String.IsNullOrEmpty(((CircleButton)Element).Icon))
+            if (e.NewElement != null)
             {
-                Control.FontFamily = new FontFamily(Abstractions.Helpers.Extensions.FindPathForFont(
-                    ((CircleButton)Element).FontIcon));
+                if (Control == null || Element == null)
+                    throw new InvalidOperationException(String.Format("Cannot convert {0} into {1}", Element.Text, typeof(Icon))); ;
 
-                IIcon icon = Abstractions.Helpers.Extensions.FindIconForKey(((CircleButton)Element).Icon,
-                       ((CircleButton)Element).FontIcon);
+                CreateCircle();
+                if (!String.IsNullOrEmpty(((CircleButton)Element).Icon))
+                {
+                    Control.FontFamily = new FontFamily(Abstractions.Helpers.Extensions.FindPathForFont(
+                        ((CircleButton)Element).FontIcon));
 
-                Control.Content = $"{icon.Character}";
-            }
-            else if (!String.IsNullOrEmpty(((CircleButton)Element).Text))
-            {
-                Control.Content = ((CircleButton)Element).Text;
+                    IIcon icon = Abstractions.Helpers.Extensions.FindIconForKey(((CircleButton)Element).Icon,
+                           ((CircleButton)Element).FontIcon);
+
+                    Control.Content = $"{icon.Character}";
+                }
+                else if (!String.IsNullOrEmpty(((CircleButton)Element).Text))
+                {
+                    Control.Content = ((CircleButton)Element).Text;
+                }
             }
         }
         /// <summary>
@@ -84,17 +87,17 @@ namespace ButtonCircle.FormsPlugin.UWP
 
         private void CreateCircle()
         {
-                var min = Math.Min(Element.Width, Element.Height);
-                Control.BorderRadius = (int)(min / 2.0);
-                Control.BorderThickness = new Windows.UI.Xaml.Thickness(Convert.ToDouble(((CircleButton)Element).BorderThickness));
-                var XFColor = ((CircleButton)Element).BorderColor;
-                Windows.UI.Color uwpColor = Windows.UI.Color.FromArgb(
-                (byte)(XFColor.A * 255),
-                (byte)(XFColor.R * 255),
-                (byte)(XFColor.G * 255),
-                (byte)(XFColor.B * 255));
-                SolidColorBrush borderColor = new SolidColorBrush(uwpColor);
-                Control.BorderBrush = borderColor;
+            var min = Math.Min(Element.Width, Element.Height);
+            Control.BorderRadius = (int)(min / 2.0);
+            Control.BorderThickness = new Windows.UI.Xaml.Thickness(Convert.ToDouble(((CircleButton)Element).BorderThickness));
+            var XFColor = ((CircleButton)Element).BorderColor;
+            Windows.UI.Color uwpColor = Windows.UI.Color.FromArgb(
+            (byte)(XFColor.A * 255),
+            (byte)(XFColor.R * 255),
+            (byte)(XFColor.G * 255),
+            (byte)(XFColor.B * 255));
+            SolidColorBrush borderColor = new SolidColorBrush(uwpColor);
+            Control.BorderBrush = borderColor;
         }
     }
 }
